a24b68f3c6d20ba247ff4066b98923f6778a742e
Rockchang <rockchang2021@163.com>
2021-09-05 15:03:45 +0800

    add tcp server and client
    1 tcp client (no read and write action)
    2 tcp server (no read and write action)

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 
+#include <unistd.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <strings.h>
+
+int tcp_client()
+{
+    //ip and addr;
+    int port = 37777;
+    char ip[] = "127.0.0.1";
+    //socket;
+    struct sockaddr_in ipaddr;
+    bzero(&ipaddr,sizeof(struct sockaddr_in));
+    ipaddr.sin_family = AF_INET;
+    ipaddr.sin_port = htons(port);
+    inet_pton(AF_INET,ip,&ipaddr.sin_addr);
+    socklen_t len = sizeof(struct sockaddr_in);
+    int cfd = socket(PF_INET,SOCK_STREAM,0);
+    if (cfd < 0)
+    {
+        return -1;
+    }
+
+    int iRet = connect(cfd,(struct sockaddr*)&ipaddr,len);
+
+    close(cfd);
+    return 0;
+}
+
+
 int main(int argc,char** argv)
 {
     std::cout << "main begin"<<std::endl;
-
+    tcp_client();
     std::cout<< "main end"<<std::endl;
     return 0;
 }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include <unistd.h>
+//#include <sys/types.h>
+//#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <strings.h>
+
+int tcp_svr()
+{
+    //ip and addr;
+    int port = 37777;
+    char ip[] = "127.0.0.1";
+    //socket;
+    struct sockaddr_in ipaddr;
+    bzero(&ipaddr,sizeof(struct sockaddr_in));
+    ipaddr.sin_family = AF_INET;
+    ipaddr.sin_port = htons(port);
+    inet_pton(AF_INET,ip,&ipaddr.sin_addr);
+
+    int sfd = socket(PF_INET,SOCK_STREAM,0);
+    if (sfd < 0)
+    {
+        return -1;
+    }
+
+    int iRet = bind(sfd,(struct sockaddr*)&ipaddr,sizeof(struct sockaddr_in));
+    if(iRet < 0)
+    {
+        return -1;
+    }
+
+    iRet = listen(sfd,5);
+    if(iRet < 0)
+    {
+        return -1;
+    }
+
+    for(;;)
+    {
+        struct sockaddr_in clientaddr;
+        socklen_t len = sizeof(struct sockaddr_in);
+        bzero(&clientaddr,len);
+        int cfd = accept(sfd,(struct sockaddr*)&clientaddr,&len);
+        if(cfd < 0)
+        {
+            continue;
+        }
+        else
+        {
+            printf("client %d\n",cfd);
+        }
+    }
+
+    close(sfd);
+    return 0;
+}
 
 int main(int argc,char** argv)
 {
     std::cout << "main begin"<<std::endl;
-
+    tcp_svr();
     std::cout<< "main end"<<std::endl;
     return 0;
 }
